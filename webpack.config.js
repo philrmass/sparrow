a4b5cd9df6f37bbd7522b1805df4f7a3d3c0d83a
Phil Mass <phil@caregiven.co>
2021-10-18 19:45:59 -0700

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   entry: './src/index.js',
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
   },
   resolve: {
