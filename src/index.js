9c978fddaebdbdb6807f6081fedcf67e6ffb673e
Phil Mass <phil@caregiven.co>
2021-10-18 22:04:07 -0700

    add eslint and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './styles/index.css';
+
 console.log('Sparrow is running');
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,30 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.(js)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
     ],
   },
   resolve: {
